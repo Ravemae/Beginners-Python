192f3401834535a93002c965aaf70d985dd43a1c
Ravemae <veraadaezeezeanya2@gmail.com>
2024-03-24 13:30:19 +0100

    Your commit message here

--- Beginners_assignments1.py
+++ Beginners_assignments1.py
@@ -4,29 +4,22 @@
 - Lastname
 - Username
 - Age
-- Phonenumber
+- Phone number
 - Password1
 - Password2 
 - Print a Message saying
     (f"Dear {Firstname} & {Lastname}, Your account has '''
-print('Welcome to ')
     
-firstname = str(input("Your Firstname\n>> ")).upper()
-lastname = str(input("Your Lastname\n>> ")).upper()
-other_name = str(input("Your Other_name\n>> ")).upper()
-email = str(input("Your email\n>> ")).upper()
-number = str(input("Your Phone Number\n>> ")).upper()
-address = str(input("Your Address\n>> ")).upper()
-dob = str(input("Your date of birth\n>> "))
+print('Welcome to Ferrostar Inc.\nPlease Fill In The Registration Form Below To Continue.😊')
+firstname = str(input("Your Firstname\n>> ")).capitalize()
+lastname = str(input("Your Lastname\n>> ")).capitalize()
+Username = str(input("Your Username\n>> ")).lower()
+Age = int(input("Your Age\n>> "))
+email = (input("Your email\n>> "))
+Phone_number = int(input("Your Phone Number\n>> "))
+Password1 = (input("Your Password1\n>> "))
+Password2 = (input("Your Password2\n>> "))
+address = int(str(input("Your Address\n>> "))).upper()
+dob = int(input("Your date of birth\n>> "))
 
-git remote add origin https://github.com/Ravemae/Beginners-Python.git
-git branch -M main
-git push -u origin main
-
-echo "# Beginners-Python" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/Ravemae/Beginners-Python.git
-git push -u origin main
+print(f"Dear {firstname} {lastname}, Your account has been created")
